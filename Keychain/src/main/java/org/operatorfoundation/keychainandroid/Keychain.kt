459145df88cfce84f8fa2b7dc6d291c7519bbfe4
CryptoSax <joshua@operatorfoundation.org>
2023-06-20 10:53:02 -0500

    Initial commit

---
+++ Keychain/src/main/java/org/operatorfoundation/keychainandroid/Keychain.kt
@@ -0,0 +1,94 @@
+package org.operatorfoundation.keychainandroid
+
+import android.util.Base64
+import androidx.security.crypto.EncryptedSharedPreferences
+import org.bouncycastle.jce.ECNamedCurveTable
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import java.security.InvalidAlgorithmParameterException
+import java.security.KeyFactory
+import java.security.KeyPairGenerator
+import java.security.NoSuchAlgorithmException
+import java.security.spec.PKCS8EncodedKeySpec
+
+class Keychain {
+    lateinit var encryptedSharedPreferences: EncryptedSharedPreferences
+
+    fun generateAndSavePrivateKey(label: String, type: KeyType): PrivateKey? {
+        return try {
+            return if (type == KeyType.P256KeyAgreement) {
+                val parameterSpec = ECNamedCurveTable.getParameterSpec("secp256r1")
+                val keyPairGenerator = KeyPairGenerator.getInstance("EC", BouncyCastleProvider())
+                keyPairGenerator.initialize(parameterSpec)
+                val privateKey = keyPairGenerator.generateKeyPair().private
+                storePrivateKey(PrivateKey.P256KeyAgreement(privateKey), label)
+                PrivateKey.P256KeyAgreement(privateKey)
+            } else {
+                println("Unsupported key type.  Returning null.")
+                null
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+            null
+        } catch (e: InvalidAlgorithmParameterException) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    fun retrievePrivateKey(label: String, type: KeyType): PrivateKey? {
+        val privateKeyString = encryptedSharedPreferences.getString(label, null)
+        if (privateKeyString == null) {
+            println("Could not find private key with provided label: $label")
+            return null
+        }
+        return if (type == KeyType.P256KeyAgreement) {
+            val keyFactory = KeyFactory.getInstance("EC", BouncyCastleProvider())
+            val privateKeyBytes = Base64.decode(privateKeyString, Base64.DEFAULT)
+            val spec = PKCS8EncodedKeySpec(privateKeyBytes)
+            val privateKey = keyFactory.generatePrivate(spec)
+            PrivateKey.P256KeyAgreement(privateKey)
+        } else {
+            println("Unsupported key type.  Returning null.")
+            null
+        }
+    }
+
+    fun deleteKey(label: String) {
+        encryptedSharedPreferences
+            .edit()
+            .remove(label)
+            .apply()
+    }
+
+    fun retrieveOrGeneratePrivateKey(label: String, type: KeyType): PrivateKey? {
+        val retrieveResult = retrievePrivateKey(label, type)
+        return if (retrieveResult != null) {
+            retrieveResult
+        } else {
+            return if (type == KeyType.P256KeyAgreement) {
+                val parameterSpec = ECNamedCurveTable.getParameterSpec("secp256r1")
+                val keyPairGenerator = KeyPairGenerator.getInstance("EC", BouncyCastleProvider())
+                keyPairGenerator.initialize(parameterSpec)
+                val privateKey = keyPairGenerator.generateKeyPair().private
+                PrivateKey.P256KeyAgreement(privateKey)
+            } else {
+                null
+            }
+        }
+    }
+
+    fun storePrivateKey(key: PrivateKey, label: String): Boolean {
+        val privateKey = when(key) {
+            is PrivateKey.P256KeyAgreement -> key.privateKey
+            else -> null
+        } ?: return false
+
+        // TODO: Verify that .encoded is correct
+        val privateKeyString = Base64.encodeToString(privateKey.encoded, Base64.DEFAULT)
+        encryptedSharedPreferences
+            .edit()
+            .putString(label, privateKeyString)
+            .apply()
+        return true
+    }
+}
---
+++ Keychain/src/main/java/org/operatorfoundation/keychainandroid/KeychainTypes.kt
@@ -0,0 +1,89 @@
+package org.operatorfoundation.keychainandroid
+
+import kotlinx.serialization.Serializable
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey
+
+enum class KeyType(val value: Int) {
+    Curve25519KeyAgreement(1),
+    P256KeyAgreement(2),
+    P384KeyAgreement(3),
+    P521KeyAgreement(4),
+
+    Curve25519Signing(5),
+    P256Signing(6),
+    P384Signing(7),
+    P521Signing(8)
+}
+
+// TODO:  add way to return key as string representation for debugging
+@Serializable
+sealed class PrivateKey {
+    class Curve25519KeyAgreement(val privateKey: java.security.PrivateKey): PrivateKey()
+    class P256KeyAgreement(val privateKey: java.security.PrivateKey) : PrivateKey()
+    class P384KeyAgreement(val privateKey: java.security.PrivateKey) : PrivateKey()
+    class P521KeyAgreement(val privateKey: java.security.PrivateKey) : PrivateKey()
+
+    class Curve25519Signing(val privateKey: java.security.PrivateKey) : PrivateKey()
+    class P256Signing(val privateKey: java.security.PrivateKey) : PrivateKey()
+    class P384Signing(val privateKey: java.security.PrivateKey) : PrivateKey()
+    class P521Signing(val privateKey: java.security.PrivateKey) : PrivateKey()
+
+    override fun toString(): String {
+        val privateKey = when(this) {
+            is P256KeyAgreement -> this.privateKey
+            else -> null
+        }
+    if (privateKey == null) {
+        println("error: invalid key type.")
+        return ""
+    }
+
+    val privateKeyBytes = privateKey.encoded
+        return bytesToHex(privateKeyBytes)
+    }
+}
+
+@Serializable
+sealed class PublicKey {
+    class Curve25519KeyAgreement(val publicKey: java.security.PublicKey): PublicKey()
+    class P256KeyAgreement(val publicKey: java.security.PublicKey) : PublicKey()
+    class P384KeyAgreement(val publicKey: java.security.PublicKey) : PublicKey()
+    class P521KeyAgreement(val publicKey: java.security.PublicKey) : PublicKey()
+
+    class Curve25519Signing(val publicKey: java.security.PublicKey) : PublicKey()
+    class P256Signing(val publicKey: java.security.PublicKey) : PublicKey()
+    class P384Signing(val publicKey: java.security.PublicKey) : PublicKey()
+    class P521Signing(val publicKey: java.security.PublicKey) : PublicKey()
+
+    override fun toString(): String {
+        val publicKey = when(this) {
+            is P256KeyAgreement -> this.publicKey
+            else -> null
+        }
+        if (publicKey == null) {
+            println("error: invalid key type.")
+            return ""
+        }
+
+        val bcecPublicKey = publicKey as BCECPublicKey
+        val point = bcecPublicKey.q
+        val encodedPoint = point.getEncoded(true)
+        val result = ByteArray(33)
+        System.arraycopy(encodedPoint, 1, result, 0, 33)
+        return bytesToHex(result)
+    }
+}
+
+fun bytesToHex(data: ByteArray): String
+{
+    val hexArray = "0123456789ABCDEF".toCharArray()
+
+    val hexChars = CharArray(data.size * 2)
+    for (j in data.indices) {
+        val v = data[j].toInt() and 0xFF
+
+        hexChars[j * 2] = hexArray[v ushr 4]
+        hexChars[j * 2 + 1] = hexArray[v and 0x0F]
+    }
+    return String(hexChars)
+}
