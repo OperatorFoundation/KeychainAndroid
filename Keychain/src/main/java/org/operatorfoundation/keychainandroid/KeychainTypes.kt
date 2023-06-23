5ec4668751aae74be989f04ad4f670ccf24fc228
CryptoSax <joshua@operatorfoundation.org>
2023-06-23 10:42:04 -0500

    added functions to publicKey

--- Keychain/src/main/java/org/operatorfoundation/keychainandroid/KeychainTypes.kt
+++ Keychain/src/main/java/org/operatorfoundation/keychainandroid/KeychainTypes.kt
@@ -3,6 +3,13 @@ package org.operatorfoundation.keychainandroid
 import android.annotation.SuppressLint
 import kotlinx.serialization.Serializable
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey
+import org.bouncycastle.jce.ECNamedCurveTable
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.jce.spec.ECParameterSpec
+import org.bouncycastle.jce.spec.ECPublicKeySpec
+import java.security.KeyFactory
+import java.security.NoSuchAlgorithmException
+import java.security.spec.InvalidKeySpecException
 
 enum class KeyType(val value: Int) {
     Curve25519KeyAgreement(1),
@@ -41,15 +48,19 @@ sealed class PrivateKey {
             return ""
         }
 
-    val privateKeyBytes = privateKey.encoded
-        return bytesToHex(privateKeyBytes)
-    }
+        val privateKeyBytes = privateKey.encoded
+            return bytesToHex(privateKeyBytes)
+        }
 }
 
 @Serializable
 sealed class PublicKey {
     class Curve25519KeyAgreement(val publicKey: java.security.PublicKey): PublicKey()
-    class P256KeyAgreement(val publicKey: java.security.PublicKey) : PublicKey()
+    class P256KeyAgreement(val publicKey: java.security.PublicKey) : PublicKey() {
+        constructor(data: ByteArray): this(bytesToPublicKey(data))
+
+        val data get() = publicKeyToBytes(publicKey)
+    }
     class P384KeyAgreement(val publicKey: java.security.PublicKey) : PublicKey()
     class P521KeyAgreement(val publicKey: java.security.PublicKey) : PublicKey()
 
@@ -57,6 +68,37 @@ sealed class PublicKey {
     class P256Signing(val publicKey: java.security.PublicKey) : PublicKey()
     class P384Signing(val publicKey: java.security.PublicKey) : PublicKey()
     class P521Signing(val publicKey: java.security.PublicKey) : PublicKey()
+
+    companion object {
+        @Throws(NoSuchAlgorithmException::class, InvalidKeySpecException::class)
+        fun bytesToPublicKey(bytes: ByteArray): java.security.PublicKey
+        {
+            if (bytes.size != 33) // This is the 33 byte key with a [0] notating the key's type and [1..] being the 32 byte key
+            {
+                throw InvalidKeySpecException()
+            }
+
+            val keyFactory = KeyFactory.getInstance("EC", BouncyCastleProvider())
+            val ecSpec: ECParameterSpec = ECNamedCurveTable.getParameterSpec("secp256r1")
+            val encodedPoint = ByteArray(33)
+            System.arraycopy(bytes, 0, encodedPoint, 1, 33)
+            encodedPoint[0] = 3
+            val point = ecSpec.curve.decodePoint(encodedPoint)
+            val pubSpec = ECPublicKeySpec(point, ecSpec)
+
+            return keyFactory.generatePublic(pubSpec)
+        }
+
+        fun publicKeyToBytes(pubKey: java.security.PublicKey?): ByteArray {
+            val bcecPublicKey = pubKey as BCECPublicKey
+            val point = bcecPublicKey.q
+            val encodedPoint = point.getEncoded(true)
+            val result = ByteArray(33)
+            System.arraycopy(encodedPoint, 1, result, 0, 33)
+
+            return result
+        }
+    }
 
     override fun toString(): String {
         val publicKey = when(this) {
